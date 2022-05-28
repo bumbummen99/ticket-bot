fd2765ce40d6cfebaec0538ed7b4f3361ea1b8b3
Patrick Henninger <privat@skyraptor.eu>
2022-05-28 21:39:08 +0200

    Amend last commit add missing config file
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

---
+++ config/database.ts
@@ -0,0 +1,28 @@
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+  // Default connection
+  connection: Env.get('DB_CONNECTION'),
+
+  // List of available connections
+  connections: {
+    pg: {
+      client: 'mysql2',
+      connection: {
+        host: Env.get('MySQL_HOST'),
+        port: Env.get('MySQL_PORT'),
+        user: Env.get('MySQL_USER'),
+        password: Env.get('MySQL_PASSWORD', ''),
+        database: Env.get('MySQL_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+  }
+}
+
+export default databaseConfig

01a80288f1c5cb23551d1e94f90211fe6d094084
Patrick Henninger <privat@skyraptor.eu>
2022-05-29 16:22:06 +0200

    Refactor register/boot of DiscordBotProvider
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

--- providers/DiscordBotProvider.ts
+++ providers/DiscordBotProvider.ts
@@ -1,31 +1,32 @@
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import Env from '@ioc:Adonis/Core/Env'
+import Logger from '@ioc:Adonis/Core/Logger'
 import { Client, Intents } from 'discord.js'
 
 export default class DiscordBotProvider {
-  constructor(protected app: ApplicationContract) {}
+  constructor(protected app: ApplicationContract) { }
 
   public register() {
     // Register your own bindings
     this.app.container.singleton('TicketBot/DiscordJS', () => {
-        /* Initialize DiscordJS */
-        const bot = new Client({
-            intents: [ Intents.FLAGS.GUILD_MEMBERS ]
-        });
-
-        /* Login with the configured token */
-        bot.login(Env.get('DISCORD_BOT_TOKEN'))
-
-        /* Attach listeners */
-        // TODO
-
-        /* Return the instance */
-        return bot
+      /* Initialize DiscordJS */
+      return new Client({
+        intents: [Intents.FLAGS.GUILD_MEMBERS]
+      })
     })
   }
 
   public async boot() {
-    // IoC container is ready
+    /* Retrieve the DiscordJS client singleton */
+    const bot: Client = this.app.container.use('TicketBot/DiscordJS')
+
+    /* Login with the configured token */
+    await bot.login(Env.get('DISCORD_BOT_TOKEN'))
+
+    /* Notify once bot is ready */
+    bot.once('ready', () => {
+      Logger.info(`Discord.js is ready!`)
+    })
   }
 
   public async ready() {
